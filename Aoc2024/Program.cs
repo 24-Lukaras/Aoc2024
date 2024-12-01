38bc65c983acc9e4c85ecb72f74e97a1b9795ad4
24-Lukaras <85632668+24-Lukaras@users.noreply.github.com>
2024-12-01 20:20:31 +0100

    Add project files.

---
+++ Aoc2024/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+using Aoc2024;
+
+using (StreamReader input = new StreamReader(@"C:\Users\Lukin\Documents\temp\aoc2024\1.txt"))
+{
+    List<string> lines = new List<string>();
+    string line;
+    while ((line = input.ReadLine()) is not null)
+    {
+        lines.Add(line);
+    }
+    var solution = Solutions.Solution_1_1(lines);
+}
---
+++ Aoc2024/Solutions.cs
@@ -0,0 +1,56 @@
+﻿namespace Aoc2024;
+
+public static class Solutions
+{
+    public static int Solution_1_0(IEnumerable<string> input)
+    {
+        int result = 0;
+
+        List<int> left = new List<int>();
+        List<int> right = new List<int>();
+
+        foreach (string line in input)
+        {
+            var arr = line.Split("   ");
+            left.Add(int.Parse(arr[0]));
+            right.Add(int.Parse(arr[1]));
+        }
+
+
+        left = left.Order().ToList();
+        right = right.Order().ToList();
+
+        for (int i = 0; i < left.Count; i++)
+        {
+            result += Math.Abs(left[i] - right[i]);
+        }
+
+        return result;
+    }
+
+    public static int Solution_1_1(IEnumerable<string> input)
+    {
+        int result = 0;
+
+        List<int> left = new List<int>();
+        Dictionary<int, int> counts = new Dictionary<int, int>();
+        foreach (string line in input)
+        {
+            var arr = line.Split("   ");
+            left.Add(int.Parse(arr[0]));
+            var right = int.Parse(arr[1]);
+            counts.TryGetValue(right, out var count);
+            counts[right] = count + 1;
+        }
+
+        foreach (var item in left)
+        {
+            if (counts.TryGetValue(item, out var count))
+            {
+                result += count * item;
+            }
+        }
+
+        return result;
+    }
+}
