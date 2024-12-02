e5b1347e436187f32d1259c4d1f613d636be806e
24-Lukaras <85632668+24-Lukaras@users.noreply.github.com>
2024-12-02 13:30:48 +0100

    day 2

--- Aoc2024/Program.cs
+++ Aoc2024/Program.cs
@@ -2,7 +2,7 @@
 
 using Aoc2024;
 
-using (StreamReader input = new StreamReader(@"C:\Users\Lukin\Documents\temp\aoc2024\1.txt"))
+using (StreamReader input = new StreamReader(@"C:\Users\Lukin\Documents\temp\aoc2024\2.txt"))
 {
     List<string> lines = new List<string>();
     string line;
@@ -10,5 +10,5 @@ using (StreamReader input = new StreamReader(@"C:\Users\Lukin\Documents\temp\aoc
     {
         lines.Add(line);
     }
-    var solution = Solutions.Solution_1_1(lines);
+    var solution = Solutions.Solution_2_1(lines);
 }
--- Aoc2024/Solutions.cs
+++ Aoc2024/Solutions.cs
@@ -1,4 +1,7 @@
-﻿namespace Aoc2024;
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Aoc2024;
 
 public static class Solutions
 {
@@ -49,6 +52,115 @@ public static class Solutions
             {
                 result += count * item;
             }
+        }
+
+        return result;
+    }
+
+    public static int Solution_2_0(IEnumerable<string> input)
+    {
+        var result = 0;
+
+        foreach (string line in input)
+        {
+            bool safe = true;
+            int direction = 0;
+            var arr = line.Split(' ').Select(int.Parse).ToArray();
+            int last = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (i == 1)
+                {
+                    if (last == arr[i])
+                    {
+                        safe = false;
+                        break;
+                    }
+
+                    direction = last > arr[i] ? 1 : -1;
+                }
+                if (i > 0)
+                {
+                    var difference = (last - arr[i]) * direction;
+                    if (difference < 1 || difference > 3)
+                    {
+                        safe = false;
+                        break;
+                    }
+                }
+                last = arr[i];
+            }
+            if (safe)
+            {
+                result++;
+            }
+        }
+
+        return result;
+    }
+
+    public static int Solution_2_1(IEnumerable<string> input)
+    {
+        var result = 0;
+
+        Func<int, int, int, bool> isOkay = (direction, x, y) =>
+        {
+            var result = (x - y) * direction;
+            return !(result < 1 || result > 3);
+        };
+        Func<int[], bool> validateSequence = (int[] sequence) =>
+        {
+            int direction = 0;
+            int last = 0;
+            for (int i = 0; i < sequence.Length; i++)
+            {
+                if (i == 1)
+                {
+                    if (last == sequence[i])
+                    {
+                        return false;
+                    }
+
+                    direction = last > sequence[i] ? 1 : -1;
+                }
+                if (i > 0)
+                {
+                    var difference = (last - sequence[i]) * direction;
+                    if (!isOkay(direction, last, sequence[i]))
+                    {
+                        return false;
+                    }
+                }
+                last = sequence[i];
+            }
+            return true;
+        };
+
+        foreach (string line in input)
+        {
+            var arr = line.Split(' ').Select(int.Parse).ToArray();
+            bool safe = false;
+
+            if (validateSequence(arr))
+                safe = true;
+
+            if (!safe)
+            {
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    var list = arr.ToList();
+                    list.RemoveAt(i);
+                    if (validateSequence(list.ToArray()))
+                    {
+                        safe = true;
+                        break;
+                    }
+                }
+            }
+
+            if (safe)
+                result++;
+
         }
 
         return result;
